67b8383dd7bf5015c411aad8e62b7058d9067d86
Brian Joseph <brianjoseph8132@gmail.com>
2025-02-19 13:48:00 +0300

    changed the string number for password to 512

--- model.py
+++ model.py
@@ -13,7 +13,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(120), nullable=False)
+    password = db.Column(db.String(512), nullable=False)
     # Relationships
     problems = db.relationship('Problem', backref='author', lazy=True)  
     solutions = db.relationship('Solution', backref='author', lazy=True)  
